f17d2e353235a0d6b1612c4d84d7b737a86093bd
Felix Krause <contact@flyx.org>
2020-07-26 13:59:48 +0200

    improved web API

--- web/web.go
+++ web/web.go
@@ -1,26 +1,61 @@
 // Package web is the API of the web interface.
 package web
 
-import "github.com/flyx/askew/runtime"
+import (
+	"encoding/json"
 
-// ConfigItem is the API of a configuration item.
-type ConfigItem interface {
-	// ID returns the identifier of this ConfigItem, which must be unique among
-	// all ConfigItems.
-	ID() string
-	// UI generates the user interface of the ConfigItem.
-	// It must create a new component for each call.
-	UI() runtime.Component
+	"github.com/flyx/askew/runtime"
+)
+
+// EditHandler is a listener that gets called when an UI element's displayed
+// state is changed.
+type EditHandler interface {
+	Edited()
+}
+
+// ConfigItemState is the state of a ConfigItem instance.
+type ConfigItemState interface {
+	// UI creates and returns this state's user interface.
+	// This method is called exactly once on each state instance.
+	UI(editHandler EditHandler) runtime.Component
+	// Reset resets the UI to the values that have last been queried via Data().
+	// If the values have never been queried, the UI is resetted to the initial
+	// data the state object was loaded with.
+	Reset()
+	// SetEnabled enables or disables the GUI.
+	SetEnabled(value bool)
+	// Data returns an object that will be serialized and sent back to the server
+	// to update the values of this ConfigItem state on the server side.
+	Data() interface{}
+}
+
+// ConfigItem describes a ConfigItem type.
+type ConfigItem struct {
+	// ID is the identifier of this ConfigItem type and must be unique among all
+	// ConfigItem instances.
+	ID string
+	// Load creates a state from the given JSON subtree.
+	// data may be nil in which case default values must be loaded.
+	Load func(data *json.RawMessage) (ConfigItemState, error)
 }
 
 // ModuleState is the API of a module's state.
+// A ModuleState acts independenly, communicating with the server on its own
+// to communicate changes. the only interface to the main application is the
+// generation of its user interface that will be placed onto the state page.
 type ModuleState interface {
-	// ID returns the identifier of this ModuleState, which must be unique among
-	// all ModuleStates.
-	ID() string
 	// UI generates the user interface of the ModuleState.
-	// It must create a new component for each call.
+	// This method is called exactly once per ModuleState instance.
 	UI() runtime.Component
+}
+
+// Module describes a module type.
+type Module struct {
+	// ID is the identifier of this Module, which must be unique among Modules
+	ID string
+	// Load creates a new ModuleState from the given JSON message, which must not
+	// be nil.
+	Load func(data *json.RawMessage) (ModuleState, error)
 }
 
 // App is the API of the web application, implemented by the QuestScreen core.
@@ -28,9 +63,9 @@ type App interface {
 	// RegisterConfigItem registers the given config item with the app.
 	// returns an error if given nil or if the given item's ID is already in use
 	// by another item.
-	RegisterConfigItem(item ConfigItem) error
+	RegisterConfigItem(item *ConfigItem) error
 	// RegisterModuleState registers the given module state with the app.
 	// returns an error if given nil or if the given item's ID is already in use
 	// by another item.
-	RegisterModuleState(item ModuleState) error
+	RegisterModuleState(item *ModuleState) error
 }
