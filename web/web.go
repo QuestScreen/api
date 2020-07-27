5e7f59c2dff66390ad4e4f00cfd700f06f48b04d
Felix Krause <contact@flyx.org>
2020-07-27 18:01:40 +0200

    better fetching interface

--- web/web.go
+++ web/web.go
@@ -39,6 +39,47 @@ type ConfigItem struct {
 	Load func(data *json.RawMessage) (ConfigItemState, error)
 }
 
+// RequestMethod is an enum of known methods for Fetch.
+type RequestMethod int
+
+const (
+	// Get is a GET request
+	Get RequestMethod = iota
+	// Post is a POST request
+	Post
+	// Put is a PUT request
+	Put
+	// Delete is a DELETE request
+	Delete
+)
+
+func (r RequestMethod) String() string {
+	switch r {
+	case Get:
+		return "GET"
+	case Post:
+		return "POST"
+	case Put:
+		return "PUT"
+	case Delete:
+		return "DELETE"
+	default:
+		panic("unknown request method!")
+	}
+}
+
+// ServerState is the interface to the server's state.
+type ServerState interface {
+	// Fetch sends an HTTP request to the server with the given method and given
+	// payload, at the path created from joining a base path with the given
+	// subpath. The result JSON is deserialized into the given target.
+	//
+	// The base path is defined by the ServerState implementation.
+	// subpath must be relative and may not begin with a slash. It may be empty.
+	// If it is not empty, it will be joined with the base path using a slash.
+	Fetch(method RequestMethod, subpath string, payload interface{}, target interface{})
+}
+
 // ModuleState is the API of a module's state.
 // A ModuleState acts independenly, communicating with the server on its own
 // to communicate changes. the only interface to the main application is the
@@ -51,15 +92,16 @@ type ModuleState interface {
 
 // Module describes a module type.
 type Module struct {
-	// ID is the identifier of this Module, which must be unique among Modules
+	// ID is the identifier of this Module, unique within the owning plugin.
 	ID string
 	// Load creates a new ModuleState from the given JSON message, which must not
-	// be nil.
-	Load func(data *json.RawMessage) (ModuleState, error)
+	// be nil. server will be a ServerState with base path "/<plugin>/<module>"
+	Load func(data *json.RawMessage, server ServerState) (ModuleState, error)
 }
 
-// App is the API of the web application, implemented by the QuestScreen core.
-type App interface {
+// PluginRegistrator is the interface for registering ConfigItems and Modules
+// of a plugin during app initialization.
+type PluginRegistrator interface {
 	// RegisterConfigItem registers the given config item with the app.
 	// returns an error if given nil or if the given item's ID is already in use
 	// by another item.
