4925dddca2ed888b968b10bd68641536728dfb16
Felix Krause <contact@flyx.org>
2021-02-05 23:09:46 +0100

    made config.Controller into config.Widget

--- web/config/controller.go
+++ web/config/controller.go
@@ -11,17 +11,15 @@ type EditHandler interface {
 	Edited()
 }
 
-// Controller backs the UI for manipulating config data in the web UI.
-type Controller interface {
+// Widget is the UI element for a ConfigItem.
+type Widget interface {
+	runtime.Component
 	// Receiver initializes the controller's state from JSON sent from the server.
 	comms.Receiver
 	// Sender sends the data entered by the user back to the server.
 	// By sending the data, it consolidates it so that future calls to Reset()
 	// will reset the values to the values being sent.
 	comms.Sender
-	// UI creates and returns this controller's user interface.
-	// This method is called exactly once on each controller instance.
-	UI(editHandler EditHandler) runtime.Component
 	// Reset resets the UI to the values that have last been queried via Data().
 	// If the values have never been queried, the UI is reset to the initial
 	// data the state object was loaded with.
